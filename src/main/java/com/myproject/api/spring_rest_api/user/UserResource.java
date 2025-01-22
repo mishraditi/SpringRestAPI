e274b3712e3c8064452d48215fe9b44fa2960525
Aditi Mishra <aditimishra91924@gmail.com>
2025-01-22 22:19:00 +0530

    Created UserResource controller and injected UserDaoService

---
+++ src/main/java/com/myproject/api/spring_rest_api/user/UserResource.java
@@ -0,0 +1,23 @@
+package com.myproject.api.spring_rest_api.user;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+public class UserResource {
+    	private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    // GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
