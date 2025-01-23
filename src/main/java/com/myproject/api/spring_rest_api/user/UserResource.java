766b632e0dd99bc5f1e801461d6ab6a4f654f72b
Aditi Mishra <aditimishra91924@gmail.com>
2025-01-23 21:24:02 +0530

    Implementing POST Method to create User Resource

--- src/main/java/com/myproject/api/spring_rest_api/user/UserResource.java
+++ src/main/java/com/myproject/api/spring_rest_api/user/UserResource.java
@@ -1,10 +1,11 @@
 package com.myproject.api.spring_rest_api.user;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class UserResource {
-    	private UserDaoService service;
+    private UserDaoService service;
 
     public UserResource(UserDaoService service) {
         this.service = service;
@@ -19,5 +20,11 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
