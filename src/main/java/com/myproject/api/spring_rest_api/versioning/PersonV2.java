9512fb2a9975cc2adb244f544860350eee67824d
Aditi Mishra <aditimishra91924@gmail.com>
2025-01-27 21:37:48 +0530

    Versioning REST API - URI Versioning

---
+++ src/main/java/com/myproject/api/spring_rest_api/versioning/PersonV2.java
@@ -0,0 +1,18 @@
+package com.myproject.api.spring_rest_api.versioning;
+
+public class PersonV2 {
+    private Name name;
+    public PersonV2(Name name) {
+        super();
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV2 [name=" + name + "]";
+    }
+}
