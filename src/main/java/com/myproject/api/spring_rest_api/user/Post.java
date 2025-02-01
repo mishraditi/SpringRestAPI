b9e1d7592228f2e2a0f7c8ed1e8732ab50d29530
Aditi Mishra <aditimishra91924@gmail.com>
2025-02-01 08:37:39 +0530

    add Post entity with many-to-one relationship to User

---
+++ src/main/java/com/myproject/api/spring_rest_api/user/Post.java
@@ -0,0 +1,37 @@
+package com.myproject.api.spring_rest_api.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Post [id=" + id + ", description=" + description + "]";
+    }
+
+}
